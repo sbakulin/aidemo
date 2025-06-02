a15809b35a196eda0b5b1eb7c998e9560da31cb8
sbakulin <serguey.bakulin@gmail.com>
2025-06-02 14:49:01 +0300

    Initial commit: React Hello World app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+      <button>hello</button>
+    </div>
+  );
+}
+
+export default App; 
